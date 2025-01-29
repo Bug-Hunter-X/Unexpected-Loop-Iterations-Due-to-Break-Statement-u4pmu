668e226d4c3d9a8fccda0f93b0346c24bd1da80d
BugHunter-X <askzenai@gmail.com>
2025-01-29 05:02:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+function myFunc() {
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+}
+//This is a more efficient and clear approach using a for loop.  The loop will run exactly 5 times.
